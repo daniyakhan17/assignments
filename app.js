9664d7095c010b69988c9eef30ff717ca00168e7
daniyakhan17 <daniyakhankhan172005@gmail.com>
2024-08-15 09:31:02 -0700

    else if

---
+++ app.js
@@ -0,0 +1,82 @@
+console.log ('hello world! 1');
+if (10<5){
+    console.log ('if chal raha hy');
+}
+else{
+    console.log ('else chal raha hy');
+}
+if(15<5){
+    console.log ('done');
+}
+else{
+    console.log('not done')
+}
+
+// var city = prompt("enter your city name");
+// if(city === "karachi"){
+//     console.log ('you live on the safest city');
+// }
+// else{
+//     console.log ("karachi ajao");
+// }
+// var country = prompt("enter country name");
+// if(country === "pakistan"){
+//     console.log ('you live in the pakistan');
+// }
+// else{
+//     console.log ('pakistan ajao')
+// }
+
+var num = 2;
+
+if(num>=20){
+    console.log("number bara hy");
+}
+else if (num < 15){
+    console.log ("number bara hy 15 se");
+}
+else {
+    console.log ("else chal raha hy")
+}
+
+
+var grade = 89;
+if (grade >= 80){
+    console.log ("A+");
+}
+
+if (grade >=70 ){
+    console.log ("A");
+}
+if (grade >= 60){
+    console.log("B");
+}
+else if (grade >= 50){
+    console.log("C");
+}
+else {
+    console.log("fail hogaya bhai");
+}
+
+
+if(10 > 5){
+    console.log("if chal raha hy");
+}
+if (20 < 50){
+    console.log("nested if");
+}
+else{
+    console.log("else chal rha hy");
+}
+
+var age = 18 ;
+if(age < 15 || age > 50){
+    console.log("admission nahi hoga");
+}
+else if (age <= 15){
+    console.log("admission miljayga");
+}
+else {
+    console.log("500 zyada do or admission loo");
+}
+
