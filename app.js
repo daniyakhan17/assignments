df0bd567ddf641ee8d96a375a6f883d9b471944d
daniyakhan17 <daniyakhankhan172005@gmail.com>
2024-08-16 08:18:57 -0700

    practice done

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-console.log ('hello world! 1');
-if (10<5){
-    console.log ('if chal raha hy');
+console.log('hello world! 1');
+if (10 < 5) {
+    console.log('if chal raha hy');
 }
-else{
-    console.log ('else chal raha hy');
-}
-if(15<5){
-    console.log ('done');
-}
-else{
-    console.log('not done')
-}
+// else {
+//     console.log('else chal raha hy');
+// }
+// if (15 < 5) {
+//     console.log('done');
+// }
+// else {
+//     console.log('not done')
+// }
 
 // var city = prompt("enter your city name");
 // if(city === "karachi"){
@@ -29,29 +29,29 @@ else{
 
 var num = 2;
 
-if(num>=20){
+if (num >= 20) {
     console.log("number bara hy");
 }
-else if (num < 15){
-    console.log ("number bara hy 15 se");
+else if (num < 15) {
+    console.log("number bara hy 15 se");
 }
 else {
-    console.log ("else chal raha hy")
+    console.log("else chal raha hy")
 }
 
 
 var grade = 89;
-if (grade >= 80){
-    console.log ("A+");
+if (grade >= 80) {
+    console.log("A+");
 }
 
-if (grade >=70 ){
-    console.log ("A");
+if (grade >= 70) {
+    console.log("A");
 }
-if (grade >= 60){
+if (grade >= 60) {
     console.log("B");
 }
-else if (grade >= 50){
+else if (grade >= 50) {
     console.log("C");
 }
 else {
@@ -59,21 +59,21 @@ else {
 }
 
 
-if(10 > 5){
+if (10 > 5) {
     console.log("if chal raha hy");
 }
-if (20 < 50){
+if (20 < 50) {
     console.log("nested if");
 }
-else{
+else {
     console.log("else chal rha hy");
 }
 
-var age = 18 ;
-if(age < 15 || age > 50){
+var age = 18;
+if (age < 15 || age > 50) {
     console.log("admission nahi hoga");
 }
-else if (age <= 15){
+else if (age <= 15) {
     console.log("admission miljayga");
 }
 else {
@@ -116,4 +116,78 @@ else {
 // console.log ("you got" + percentage + "%");
 
 
+// FUNCTIONS
+
+function hello() {
+    console.log("hello world");
+    console.log("hello world");
+    console.log("hello world");
+    console.log("hello world");
+}
+
+hello();
+hello();
+
+function greetuser(username) {
+    console.log("hello" + username);
+}
+
+greetuser('abdullah');
+greetuser("daniya");
+greetuser("baba");
+greetuser();
+
+function sum(num1, num2) {
+    console.log(num1 + num2);
+}
+sum("20", "20");
+
+function greetuser(username) {
+    console.log("hello" + username);
+    return "abc"
+}
+
+// hello kuch bhi nhi
+
+console.log(greetuser("a"));
+
+// var username = prompt("enter your name");
+// console.log(username);
+
+var greet = "20";
+console.log(greet);
+
+function addition(num1, num2) {
+    return num1 + num2;
+}
+
+var result = addition(20, 80);
+console.log(result);
+
+function calculatepercentage(num1, num2, num3, num4) {
+    var obtainedMarks = num1 + num2 + num3 + num4;
+    var percentage = obtainedMarks / 400 * 100;
+    return percentage;
+}
+
+console.log(calculatepercentage(20, 30, 40, 100));
+
+var head = document.querySelector("#head");
+head.innerHTML = "change by me";
+
+// var para = document.querySelector("#para");
+// var div = document.querySelector("div")
+
+// para.innerHTML = "Lorem ipsum, <br/> dolor sit amet consectetur adipisicing elit. Consectetur, harum."
+
+
+// var para = document.querySelector ("#para");
+
+// function change para () {
+//     if (para.innerHTML === "hello world"){
+//         para.innerHTML = "hello para"
+//     }else{
+//         para.innerHTML = "hello world"
+//     }
+// }
 
